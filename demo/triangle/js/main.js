40ce61b41bbcbaf5f414ea41c2e71d2203b75525
createdevelopsolutions <whrthrislight@hotmail.com>
2015-04-20 01:35:20 +0800

    Added files to triangleplayer.

--- demo/triangle/js/main.js
+++ demo/triangle/js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
   var apiKey = 'AIzaSyAZpGD3Fua2rc074DtcUVuSsZH4OUauFy4';
 
   var no = 2;
+  var medialist = {};
+
+  var hasSelected = false;
 
   function populateItems (item) {
     // Gets the latest row's row-items length
@@ -47,6 +50,18 @@ $(document).ready(function () {
       $( $('.row')[ $('.row').length - 1 ] ).append('<div class="row-item' + flip + '"></div>');
       console.log('new');
     }
+
+    var mobileRowList = $('.triangle-holder-mobile').find('.row-mobile');
+    mobileRowList = mobileRowList[ mobileRowList.length - 1 ];
+
+    $(mobileRowList).append('<a data-id="' + item.contentDetails.videoId + '" class="col-md-3" style="background-image: url(' + thumbnails.url + ');" href="#">' +
+      '<span>' + item.snippet.title + '</span></a>');
+
+    if ($(mobileRowList).find('.col-md-3').length === 4) {
+      $('.triangle-holder-mobile').append('<div class="row-mobile"></div>');
+    }
+
+    medialist[item.contentDetails.videoId] = item.snippet;
   }
 
   function getPlaylists (channelName, defer) {
@@ -84,10 +99,9 @@ $(document).ready(function () {
           }
 
           $('.row:last-child').remove();
-
-          $('#title').html('Select a video on the right to play');
           $('.triangle-holder').slideDown(2000);
-          $('#title').addClass('border');
+
+          $( $('.triangle')[0] ).click();
         });
       }
     });
@@ -97,29 +111,57 @@ $(document).ready(function () {
 
   $('.triangle-holder').on('click', '.triangle', function () {
     $('#display').attr('src', 'https://www.youtube.com/embed/' + $(this).attr('id') +
-      '?autoplay=1&controls=0&showinfo=0&disablekb=0');
-    $('#title').html( '<span class="play-button"></span><span class="play-button right"></span> Playing - ' + $(this).attr('title') );
-
-    if ($('#display').css('position') === 'static') {
-      $('#display').slideDown();
-      $('#title').removeClass('border');
-    }
+      '?autoplay=1&controls=0&showinfo=0&disablekb=0&enablejsapi=1&modestbranding=0&iv_load_policy=0');
+    $('#title').find('small').html( 'Playing - ' + medialist[$(this).attr('id')].title + ' <button id="info-btn">More Info . . .</button>');
+    $('#content').find('p').html( medialist[$(this).attr('id')].description );
+    $('#content').find('h3 small').html( new Date(medialist[$(this).attr('id')].publishedAt).toString() );
   });
 
-  $('#get-channel').keyup(function (event) {
+  $('.triangle-holder-mobile').on('click', '.col-md-3', function () {
+    $('#display').attr('src', 'https://www.youtube.com/embed/' + $(this).attr('data-id') +
+      '?autoplay=1&controls=0&showinfo=0&disablekb=0&enablejsapi=1&modestbranding=0&iv_load_policy=0');
+    $('#title').find('small').html( 'Playing - ' + medialist[$(this).attr('data-id')].title + ' <button id="info-btn">More Info . . .</button>');
+    $('#content').find('p').html( medialist[$(this).attr('data-id')].description );
+    $('#content').find('h3 small').html( new Date(medialist[$(this).attr('data-id')].publishedAt).toString() );
+    resize();
+    $('#title small').addClass('selected');
+    $('.triangle-holder-mobile').hide();
+  });
+
+  $('#title small').on('click', '#info-btn', function () {
+    $(this).html( $('#content').is(':visible') ? 'More Info . . .' : 'Less Info . . .' );
+    $('#content').slideToggle();
+  });
+
+  $('#menu-btn').click(function () {
+    $('.triangle-holder-mobile').slideToggle();
+    resize();
+  });
+
+  /*$('#get-channel').keyup(function (event) {
     if (event.keyCode == '13') {
        getPlaylists($(this).val());
     }
-  });
+  });*/
+  getPlaylists('lindseystomp');
 
   function resize () {
-    $('#title').css('width', ($(window).innerWidth - 50) + 'px');
+    $('#title').css('width', ($(window).innerWidth() - 50) + 'px');
 
     if ($('#display').css('position') === 'static') {
       $('#display').css('height', ($(window).innerWidth() * 0.565) + 'px');
     } else {
       $('#display').css('height', '100%');
     }
+
+    var titleHeight = $('#title').outerHeight();
+    var fixedPx = ($(window).innerHeight() - titleHeight - 50) + 'px';
+
+    $('#content').css('height', fixedPx);
+    $('#content').css('max-height', fixedPx);
+    $('.triangle-holder-mobile').css('height', fixedPx);
+    $('.triangle-holder-mobile').css('max-height', fixedPx);
+    $('.triangle-holder-mobile').css('top', titleHeight + 'px');
   }
 
   $(window).resize(resize);
@@ -128,7 +170,22 @@ $(document).ready(function () {
     if ($('#display').css('position') === 'static' && $(window).scrollTop() > 0) {
       $('#title.border').show();
     }
+
+    console.log('data', ($(window).innerHeight() - $('#title').outerHeight()));
   });
 
   resize();
+
+  $('#navigation a').click(function () {
+    $('.section').hide();
+    $('#display').attr('src', '');
+    $('#title small').html('');
+    $( '#' + $(this).attr('data-target') ).fadeIn();
+    $('#navigation a').removeClass('active');
+    $(this).addClass('active');
+    $('body').attr('class', '');
+    $('body').addClass( $(this).attr('data-target').split('section-')[1] );
+
+    resize();
+  })
 });
